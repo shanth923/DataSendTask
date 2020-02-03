f59ebc470af1cd3a811645c3994f9d6e3293443d
shanth kumar <shanth923@gmail.com>
2020-02-03 10:48:45 +0530

    Initial Commit

---
+++ DataSendTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataSendTask
+//
+//  Created by Vidushi Duhan on 03/02/20.
+//  Copyright © 2020 Vidushi Duhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
