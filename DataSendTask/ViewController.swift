7a96abf5c443ee6c6da4104737e385b99db3ac85
shanth kumar <shanth923@gmail.com>
2020-02-07 18:22:30 +0530

    added

--- DataSendTask/ViewController.swift
+++ DataSendTask/ViewController.swift
@@ -12,9 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
 
 
+    
+    func dataRequest(){
+        
+        
+       
+        
+        
+        
+    }
+    
+   
+    
 }
 
